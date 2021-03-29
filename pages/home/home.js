5de7e552052eaf61dda73ff78cd9f811038a6915
unknown <2747254700@qq.com>
2021-03-29 16:47:17 +0800

    登录与分享

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,43 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  handleShowToast () {
+    wx.showToast({
+      title: '哈哈哈',
+      duration: 3000,
+      mask: true
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleShowModel () {
+    wx.showModal({
+      title: '标题',
+      content: '内容',
+      showCancel: false
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleShowLoading () {
+    wx.showLoading({
+      title: '加载中',
+    })
+    setTimeout(() => {
+      wx.hideLoading({
+        success: (res) => {},
+      })
+    }, 1000)
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleShowAction () {
+    wx.showActionSheet({
+      itemList: ['拍照', '相册'],
+      success: (index) => {
+        console.log(index);
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 分享
+  onShareAppMessage (options) {
+    return {
+      title: '分享界面',
+      imageUrl: 'https://stylus.bootcss.com/img/stylus-logo.png',
+      path: '/pages/about/about'
+    }
   }
 })
